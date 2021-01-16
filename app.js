7008ee65d3f011a004595692f2e5cffdee3483c3
Viacheslav Orel <trosha2306@gmail.com>
2021-01-16 22:26:55 +0200

    Finish hometask4

--- app.js
+++ app.js
@@ -1,23 +1,37 @@
 const express = require('express');
 const path = require('path');
 
+
 const app = express();
 const port = 9090;
+
+// middleware ----------------------------------------------------------------
+const cookiesParser = require('cookie-parser');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookiesParser());
 
 // Static data -------------------------------------------------------------
 const jsPath = path.join(__dirname, 'public', 'javascript');
 const cssPath = path.join(__dirname, 'public', 'stylesheets');
 
+
 app.use(express.static(jsPath));
 app.use(express.static(cssPath));
-app.use(express.json());
 
 // Game route --------------------------------------------------------------
 const home = require('./controllers/home');
+const login = require('./controllers/login');
+const logout = require('./controllers/logout');
 const getResults = require('./controllers/getResult');
 const setResult = require('./controllers/setResult');
 
 app.get('/', home);
+
+app.post('/login', login);
+app.get('/logout', logout);
+
 app.get('/results', getResults);
 app.post('/results', setResult);
 
@@ -36,5 +50,10 @@ app.post('/api/tasks/brackets', brackets);
 app.post('/api/tasks/arraySort', arraySort);
 app.post('/api/tasks/nextIndex', nexIndex);
 
+
+// errors ----------------------------------------------------------------------
+const errorHandler = require('./controllers/error');
+
+app.use(errorHandler);
 
 app.listen(port);
--- controllers/home.js
+++ controllers/home.js
@@ -1,5 +1,12 @@
 const path = require('path');
 
+const pathToGame = path.join(__dirname, '../controllers/..', 'bin', 'index.html');
+const pathToLigin = path.join(__dirname, '../controllers/..', 'bin', 'login.html')
+
 module.exports = function(req, res) {
-    res.sendFile(path.join(__dirname, '../controllers/..', 'bin', 'index.html'));
+    if (req.cookies.isAuth === 'yes') {
+        res.sendFile(pathToGame);
+    } else {
+        res.sendFile(pathToLigin);
+    }
 }
